dfee1debf4851635fa88633dd3a5573c8bbbbe8e
Bela-Mina <belaandrosbela@gmail.com>
2025-09-14 23:23:45 +0300

    Add 9 script to sum two integers using a function

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+
+// Adds two integers passed as command-line arguments
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const arg1 = parseInt(process.argv[2]);
+  const arg2 = parseInt(process.argv[3]);
+  
+  console.log(add(arg1, arg2));
+  
